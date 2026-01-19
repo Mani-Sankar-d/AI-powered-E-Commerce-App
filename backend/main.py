230790c822a81b335455153417da5ab758fd473c
Mani-Sankar-d <manisankard67@gmail.com>
2026-01-19 12:05:08 +0530

    deploy-try-1

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,6 @@ import asyncio
 @app.on_event("startup")
 async def startup():
     await init_db()
-# @app.on_event("startup")
-# async def start_worker():
-#     asyncio.create_task(worker_loop())
-#     print("🚀 Worker task started")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)

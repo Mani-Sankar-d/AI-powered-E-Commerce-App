e417c15216693a3f9f45df5701c6d81a7f5535e8
Mani-Sankar-d <manisankard67@gmail.com>
2026-01-19 13:07:42 +0530

    deploy-try-8

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,8 @@
 # main.py
 import uvicorn
 from app import app
-from db_init import init_db
 import asyncio
-@app.on_event("startup")
-async def startup():
-    await init_db()
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
